4ae40a3396c754f4d330dbc2ccb20c32b102d480
Vinzzt <97930168+Kevinafsarmanik@users.noreply.github.com>
2022-05-15 13:07:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['6281390658325'],
-  ['6282256115584'],
-  ['6281319944687', 'BG', true]
+  ['6282132156123'],
+  ['6282132156123'],
+  ['6282132156123', 'BG', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
-global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.mods = [6282132156123] // Want some help?
+global.prems = [6282132156123] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -30,8 +30,8 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.packname = 'wa.me/6285713964963'
-global.author = 'Metro Bot'
+global.packname = 'wa.me/6282132156123'
+global.author = 'Xhamster Bot'
 
 global.multiplier = 69 // The higher, The harder levelup
 
